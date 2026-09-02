56763554bda80c965e225e53eb7e6a7cb47ec465
MTPeraya <m.perayalsc@gmail.com>
2026-09-02 14:28:12 +0700

    Setup: Initial project structure for Spring Boot(Java) Backend setup

---
+++ transaction-backend/src/test/java/com/mtperaya/transactionbackend/TransactionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtperaya.transactionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
